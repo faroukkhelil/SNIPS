3a48b77c27f882ef075d0a32c43eb974b8604f12
faroukkhelil <39222112+faroukkhelil@users.noreply.github.com>
2018-09-05 13:25:52 +0100

    Update browsUrl.py

--- browsUrl.py
+++ browsUrl.py
@@ -1,53 +1,34 @@
-#!/usr/bin/env python2
-
+# !/usr/bin/env python
+# encoding: utf-8
+import paho.mqtt.client as mqtt
+import json
 import webbrowser
-import sys
 
-from hermes_python.hermes import Hermes
+def on_connect(client, userdata, flags, rc):
+    print('Connected')
+    mqtt.subscribe('hermes/intent/#')
+def on_message(client, userdata, msg):
+    url="http://www.google.fr/"
+    intent_json = json.loads(msg.payload)
+    intentName = intent_json['intent']['intentName']
+    slots = intent_json['slots']
+    #print('Intent {}'.format(intentName))
+    opt=""
+    for slot in slots:
+        slot_name = slot['slotName']
+        raw_value = slot['rawValue']
+        value = slot['value']['value']
+        if slot_name == "ref_num" or slot_name == "numero_devis" :
+                value = str(int(float(value)))
+        #print('Slot {} -> \n\tRaw: {} \tValue: {}'.format(slot_name, raw_value, value))
+        if opt == "" :
+                opt=slot_name+"="+value
+        else :
+                opt=opt+"&"+slot_name+"="+value
+    webbrowser.open(url+'?'+opt, new=2)
 
-MQTT_IP_ADDR = "localhost"
-MQTT_PORT = 1883
-MQTT_ADDR = "{}:{}".format(MQTT_IP_ADDR, str(MQTT_PORT))
-
-def intent_received(hermes, intent_message):
-    sentence = 'You asked for '
-    print('Message recu')
-    if intent_message.intent.intent_name == 'fkhelil:demande_client':
-        print('Demande de Fiche Client')
-        sentence += 'Client '
-        opt=''
-        for i in range(2,len(sys.argv)) :
-            if  i % 2 == 0 :
-                opt=opt+sys.argv[i]+'='
-            else :
-                if i == len(sys.argv)-1 :
-                    opt=opt+sys.argv[i]
-                else :
-                    opt=opt+sys.argv[i]+'&'
-        url=sys.argv[1]
-        webbrowser.open(url+'?'+opt, new=2)
-    elif intent_message.intent.intent_name == 'fkhelil:demande_devis':
-        print('Consultation de Devis')
-        sentence += 'Devis '
-    elif intent_message.intent.intent_name == 'fkhelil:demande_produit':
-        print('Demande de Fiche Produit')
-        sentence += 'Product '
-    else:
-        return
-
-    forecast_country_slot = intent_message.slots.forecast_country.first()
-    forecast_locality_slot = intent_message.slots.forecast_locality.first()
-    forecast_start_datetime_slot = intent_message.slots.forecast_start_datetime
-
-    if forecast_locality_slot is not None:
-        sentence += 'in ' + forecast_locality_slot.value
-    if forecast_country_slot is not None:
-        sentence += 'in ' + forecast_country_slot.value
-    if forecast_start_datetime_slot is not None and len(forecast_start_datetime_slot) > 0:
-        sentence += ' ' + forecast_start_datetime_slot[0].raw_value
-
-    hermes.publish_end_session(intent_message.session_id, sentence)
-
-
-with Hermes(MQTT_ADDR) as h:
-    h.subscribe_intents(intent_received).start()
+mqtt = mqtt.Client()
+mqtt.on_connect = on_connect
+mqtt.on_message = on_message
+mqtt.connect('raspberrypi.local', 1883)
+mqtt.loop_forever()
